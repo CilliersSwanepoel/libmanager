1376c829c469841bee3c266aa60ec778a2e3c604
Cilliers Swanepoel <chrisswa@outlook.com>
2024-08-31 12:15:47 +0200

    Created AWS endpoint

--- libmanager-backend/db.js
+++ libmanager-backend/db.js
@@ -2,7 +2,7 @@ const { Pool } = require('pg');
 
 const pool = new Pool({
     user: 'postgres',       // Replace with your PostgreSQL username
-    host: 'localhost',           // Hostname of your PostgreSQL server
+    host: 'libmanager.czo8aeqgqkfw.eu-north-1.rds.amazonaws.com',           // Hostname of your PostgreSQL server
     database: 'libmanager',  // Your database name
     password: 'Quxub7164*02',   // Replace with your PostgreSQL password
     port: 5432,                  // Default PostgreSQL port
