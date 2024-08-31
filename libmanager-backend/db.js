b82634485b9baaf3e26e5d350db616a2966189ab
Cilliers Swanepoel <chrisswa@outlook.com>
2024-08-31 13:36:55 +0200

    Update db.js

--- libmanager-backend/db.js
+++ libmanager-backend/db.js
@@ -1,18 +1,16 @@
 const { Client } = require('pg');
 
-// Create a new PostgreSQL client instance
 const client = new Client({
-    user: 'postgres',       // Your PostgreSQL username
-    host: 'libmanager.czo8aeqgqkfw.eu-north-1.rds.amazonaws.com', // Your AWS RDS endpoint
-    database: 'libmanager', // Your database name
-    password: 'Quxub7164*02',   // Your PostgreSQL password
-    port: 5432,             // Default PostgreSQL port
+    user: 'postgres',    
+    host: 'libmanager.czo8aeqgqkfw.eu-north-1.rds.amazonaws.com', 
+    database: 'libmanager', 
+    password: 'Quxub7164*02',  
+    port: 5432,            
     ssl: {
-        rejectUnauthorized: false, // Disable SSL certificate validation for development
+        rejectUnauthorized: false,
     },
 });
 
-// Connect to the PostgreSQL database
 client.connect()
     .then(() => console.log('Connected to AWS PostgreSQL database successfully'))
     .catch(err => console.error('Connection error', err.stack));
