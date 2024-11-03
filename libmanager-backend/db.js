3c25eb877e80d27c035742ceed97eea98e581cb9
Cilliers Swanepoel <chrisswa@outlook.com>
2024-11-03 18:16:53 +0200

    Update db.js

--- libmanager-backend/db.js
+++ libmanager-backend/db.js
@@ -2,7 +2,7 @@ const { Client } = require('pg');
 
 const client = new Client({
     user: 'postgres',    
-    host: 'libmanager.czo8aeqgqkfw.eu-north-1.rds.amazonaws.com', 
+    host: 'localhost', 
     database: 'libmanager', 
     password: 'Quxub7164*02',  
     port: 5432,            
@@ -12,7 +12,7 @@ const client = new Client({
 });
 
 client.connect()
-    .then(() => console.log('Connected to AWS PostgreSQL database successfully'))
+    .then(() => console.log('Connected to PostgreSQL database successfully'))
     .catch(err => console.error('Connection error', err.stack));
 
 module.exports = client;
